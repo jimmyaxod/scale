c9a019cb2a6e19e1e97cbf186644e3e654def4cb
Jimmy Moore <jamesmoore@loopholelabs.io>
2023-01-17 19:30:47 +0000

    Removed unused code

--- ts/compile/builder/crates/quickjs-wasm-rs/src/lib.rs
+++ ts/compile/builder/crates/quickjs-wasm-rs/src/lib.rs
@@ -1,11 +1,4 @@
 mod js_binding;
-mod serialize;
 
 pub use crate::js_binding::context::Context;
 pub use crate::js_binding::value::Value;
-
-#[cfg(feature = "messagepack")]
-pub mod messagepack;
-
-#[cfg(feature = "json")]
-pub mod json;
